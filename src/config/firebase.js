f27fe08bbdd80ae856ec8f5359c6caf01e7c924e
sarriaza95 <s.arriaza95@gmail.com>
2025-11-26 15:14:20 -0600

    deploy en render

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,4 @@
-const admin = require('firebase-admin');
+/* const admin = require('firebase-admin');
 const path = require('path');
 const dotenv = require('dotenv');
 
@@ -8,6 +8,29 @@ const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT || './serviceAcc
 const serviceAccount = require(path.resolve(serviceAccountPath));
 
 if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+const db = admin.firestore();
+
+module.exports = { admin, db }; */
+
+/*configuracion para publicacion en render*/
+
+// src/config/firebase.js
+const admin = require('firebase-admin');
+
+if (!admin.apps.length) {
+  const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+  if (!serviceAccountJson) {
+    throw new Error('FIREBASE_SERVICE_ACCOUNT env var is not set');
+  }
+
+  const serviceAccount = JSON.parse(serviceAccountJson);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
